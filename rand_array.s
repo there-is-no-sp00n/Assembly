7809514cbd816af40dd7c3b4637308182ea887bb
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-11-30 10:33:43 -0600

    Update rand_array.s

--- rand_array.s
+++ rand_array.s
@@ -70,6 +70,7 @@ _seedrand:
     MOV R0, #0              @ pass 0 as argument to time call
     BL time                 @ get system time
     MOV R1, R0              @ pass sytem time as argument to srand
+    BL _mod_unsigned
     BL srand                @ seed the random number generator
     POP {PC}                @ return 
     
@@ -77,6 +78,23 @@ _getrand:
     PUSH {LR}               @ backup return address
     BL rand                 @ get a random number
     POP {PC}                @ return 
+    
+
+_mod_unsigned:
+    @cmp R2, R1          @ check to see if R1 >= R2
+    @MOVHS R0, R1        @ swap R1 and R2 if R2 > R1
+    @MOVHS R1, R2        @ swap R1 and R2 if R2 > R1
+    @MOVHS R2, R0        @ swap R1 and R2 if R2 > R1
+    @MOV R0, #0          @ initialize return value
+    B _modloopcheck     @ check to see if
+    _modloop:
+        ADD R0, R0, #1  @ increment R0
+        SUB R1, R1, #1000  @ subtract R2 from R1
+    _modloopcheck:
+        CMP R1, R2      @ check for loop termination
+        BHS _modloop    @ continue loop if R1 >= R2
+    MOV R0, R1          @ move remainder to R0
+    MOV PC, LR 
    
 .data
 
