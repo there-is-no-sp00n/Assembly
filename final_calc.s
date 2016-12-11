bca55f969340a2b189968faa76ec798c5bec811e
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-12-11 09:57:21 -0600

    Update final_calc.s

--- final_calc.s
+++ final_calc.s
@@ -25,7 +25,7 @@ _where_to_go:
 
 _abs:
     VMOV S0, R1             @ move return value R0 to FPU register S0
-    VABS S0, S0
+    VABS.F32 S0, S0
     VCVT.F64.F32 D1, S0     @ covert the result to double precision for printing
     VMOV R1, R2, D1         @ split the double VFP register into two ARM registers
     BL _printf
