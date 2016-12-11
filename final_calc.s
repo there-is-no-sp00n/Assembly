d5ec95d0a57f90d65db1224e99a5cc9eeb8f54ba
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-12-11 09:41:40 -0600

    Create final_calc.s

---
+++ final_calc.s
@@ -0,0 +1,46 @@
+@ CSE 2312
+@ Final Calculator
+@ Final Progamming Assignment
+
+
+.global main
+.func main
+
+main:
+    BL _scanf
+    MOV R1, R0
+    BL get_char
+    MOV R2, R0
+
+
+
+_scanf:
+    PUSH {LR}               @ store LR since scanf call overwrites
+    SUB SP, SP, #4          @ make room on stack
+    LDR R0, =format_str     @ R0 contains address of format string
+    MOV R1, SP              @ move SP to R1 to store entry on stack
+    BL scanf                @ call scanf
+    LDR R0, [SP]            @ load value at SP into R0
+    ADD SP, SP, #4          @ restore the stack pointer
+    POP {PC}                @ return
+
+
+get_char:	
+	MOV R7, #3			@ set the mode to 3
+	MOV R0, #0			@ input stream is 0, the monitor
+	MOV R2, #1			@ read a single char
+	LDR R1, = read_char		@ store the character in data memory
+	SWI 0				@ execute the system call
+	LDR R0, [R1]			@ move the character to the return register
+	AND R0, #0xFF			@ mask out everything but the right 8 bits
+	MOV PC, LR			@ return
+
+
+
+.data
+
+read_char:    .asciz  " "
+format_str:   .asciz  "%f"
+
+
+.end
