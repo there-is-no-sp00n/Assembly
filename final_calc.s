669cc65d7b3ee61287b8f8270c086daa50c6d7c0
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-12-11 10:56:35 -0600

    Update final_calc.s

--- final_calc.s
+++ final_calc.s
@@ -8,12 +8,9 @@
 
 main:
     BL _scanf
-    @MOV R1, R0
     PUSH {R0}
     BL get_char
-    @POP {R1}
     MOV R2, R0
-    @BL _iprint
     BL execute_calc
     
 
@@ -46,11 +43,12 @@ _abs:
     VABS.F32 S0, S0
     VCVT.F64.F32 D1, S0     @ covert the result to double precision for printing
     VMOV R1, R2, D1         @ split the double VFP register into two ARM registers
-    @VMOV R1, S0
     BL _printf
     B main
     
 _sqr_root:
+	LDR R0, =sqrt_str
+	BL printf
 	POP {R1}
 	VMOV S0, R1
 	VSQRT.F32 S0, S0
@@ -66,14 +64,7 @@ _printf:
     BL printf               @ call printf
     POP {PC}                @ pop LR from stack and return
 
-@_iprint:
-@	PUSH {LR}
-@	LDR R0, =output_str
-@	VMOV S0, R1             @ move return value R0 to FPU register S0
- @   VCVT.F64.F32 D1, S0     @ covert the result to double precision for printing
-  @  VMOV R1, R2, D1         @ split the double VFP register into two ARM registers
-@	BL printf
-@	POP {PC}
+
 
 _scanf:
     PUSH {LR}               @ store LR since scanf call overwrites
@@ -94,7 +85,8 @@ _scanf:
 read_char:    .asciz  " "
 format_str:   .asciz  "%f"
 abs_str:	.asciz	"Absolute is: " 
+sqrt_str:	.asciz	"Sqaure root is: "
 output_str:   .asciz  "%f\n"
-printf_str:     .asciz      " %f\n"
+printf_str:     .asciz      "%f\n"
 
 .end
