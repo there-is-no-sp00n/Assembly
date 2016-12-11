24edaa99067eeb79f7bd02276ae08a2e3592179a
there-is-no-sp00n <aninda.zaman@gmail.com>
2016-12-11 11:16:59 -0600

    Update final_calc.s

--- final_calc.s
+++ final_calc.s
@@ -62,7 +62,7 @@ _pow:
 	MOV R3, R0
 	POP {R1}
 	VMOV S0, R1
-loop:
+lop:
 	CMP R3, #1
 	VCVTEQ.F64.F32 D1, S0     @ covert the result to double precision for printing
     	VMOVEQ R1, R2, D1         @ split the double VFP register into two ARM registers
@@ -71,7 +71,7 @@ loop:
 		
 	VMUL.F32 S0, S0, S0
 	SUB R3, R3, #1
-	B loop
+	B lop
 	
     
 _printf:
